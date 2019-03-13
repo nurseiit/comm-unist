0b4342641446392dd57bf830cadb0ddb93ddc607
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-03-13 21:56:29 +0900

    Add name & id

--- datalab/bits.c
+++ datalab/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * <Nurseiit Abdimomyn 20172001>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
