fbdd52f934ad90acb619412f666240685683ed50
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-03-17 00:27:28 +0900

    Fix `uni06` compilation error.

--- lab1/DoublyLinkedList.cc
+++ lab1/DoublyLinkedList.cc
@@ -4,3 +4,129 @@
 
 using namespace std;
 
+DoublyLinkedList::DoublyLinkedList() {
+  length = 0;
+  Front = Back = NULL;
+}
+
+/*
+ * returns –
+ *    true: if empty
+ *    false: otherwise
+ */
+bool DoublyLinkedList::empty() const {
+  return length == 0;
+}
+
+/*
+ * returns the first element of the list
+ * throws `runtime_error` exception if the list is empty
+ */
+int& DoublyLinkedList::front() const {
+  if (empty()) throw runtime_error("List is Empty!");
+  return Front->value;
+};
+
+/*
+ * returns the last element of the list
+ * throws `runtime_error` exception if the list is empty
+ */
+int& DoublyLinkedList::back() const {
+  if (empty()) throw runtime_error("List is Empty!");
+  return Back->value;
+}
+
+/*
+ * inserts an item at the beginning of the list
+ */
+void DoublyLinkedList::add_front(const int& e) {
+  Linked *now = new Linked(e);
+  if (empty()) {
+    Front = Back = now;
+  } else {
+    now->right = Front;
+    Front->left = now;
+    Front = now;
+  }
+  length += 1;
+}
+
+/*
+ * removes the first element 
+ * throws `runtime_error` exception if the list is empty
+ */
+void DoublyLinkedList::remove_front() {
+  if (empty()) throw runtime_error("List is Empty!");
+  Linked *temp = Front;
+  Front = Front->right;
+  if (Front)
+    Front->left = NULL;
+  length -= 1;
+  free(temp);
+}
+
+/*
+ * inserts an item at the back of the list
+ */
+void DoublyLinkedList::add_back(const int& e) {
+  Linked *now = new Linked(e);
+  if (empty()) {
+    Front = Back = now;
+  } else {
+    now->left = Back;
+    Back->right = now;
+    Back = now;
+  }
+  length += 1;
+}
+
+/*
+ * removes the last element
+ * throws `runtime_error` exception if the list is empty
+ */
+void DoublyLinkedList::remove_back() {
+  if (empty()) throw runtime_error("List is Empty!");
+  Linked *temp = Back;
+  Back = Back->left;
+  if (Back)
+    Back->right = NULL;
+  length -= 1;
+  free(temp);
+}
+
+/*
+ * returns the number of items in the list
+ */
+size_t DoublyLinkedList::get_size() const {
+  return length;
+}
+
+/*
+ * returns a string representation of the list in order.
+ * e.g., for a list [1,2,3,4], 
+ * 1 2 3 4
+ */
+string DoublyLinkedList::to_string() {
+  string result = "";
+  Linked* now = Front;
+  while (now) {
+    result += ('0' + now->value);
+    now = now->right;
+  }
+  return result;
+};
+
+/*
+ * returns a string representation of the list in reverse order.
+ * e.g., for a list [1,2,3,4], 
+ * 4 3 2 1
+ */
+string DoublyLinkedList::to_reverse_string() {
+  string result = "";
+  Linked* now = Back;
+  while (now) {
+    result += ('0' + now->value);
+    now = now->left;
+  }
+  return result;
+};
--- lab1/DoublyLinkedList.h
+++ lab1/DoublyLinkedList.h
@@ -1,8 +1,6 @@
 #ifndef __DOUBLY_LINKED_LIST_H__
 #define __DOUBLY_LINKED_LIST_H__
 
-using namespace std;
-
 #include "List.h"
 
 class Linked {
@@ -21,131 +19,26 @@ class DoublyLinkedList: public List {
   size_t length;
 
   public:
-    DoublyLinkedList() {
-      length = 0;
-      Front = Back = NULL;
-    }
+    DoublyLinkedList();
 
-    /*
-     * returns –
-     *    true: if empty
-     *    false: otherwise
-     */
-    bool empty() const {
-      return length == 0;
-    }
+    bool empty() const;
 
-    /*
-     * returns the first element of the list
-     * throws `runtime_error` exception if the list is empty
-     */
-    int& front() const {
-      if (empty()) throw runtime_error("List is Empty!");
-      return Front->value;
-    };
+    int& front() const;
 
-    /*
-     * returns the last element of the list
-     * throws `runtime_error` exception if the list is empty
-     */
-    int& back() const {
-      if (empty()) throw runtime_error("List is Empty!");
-      return Back->value;
-    }
+    int& back() const;
 
-    /*
-     * inserts an item at the beginning of the list
-     */
-    void add_front(const int& e) {
-      Linked *now = new Linked(e);
-      if (empty()) {
-        Front = Back = now;
-      } else {
-        now->right = Front;
-        Front->left = now;
-        Front = now;
-      }
-      length += 1;
-    }
+    void add_front(const int& e);
 
-    /*
-     * removes the first element 
-     * throws `runtime_error` exception if the list is empty
-     */
-    void remove_front() {
-      if (empty()) throw runtime_error("List is Empty!");
-      Linked *temp = Front;
-      Front = Front->right;
-      if (Front)
-        Front->left = NULL;
-      length -= 1;
-      free(temp);
-    }
+    void remove_front();
 
-    /*
-     * inserts an item at the back of the list
-     */
-    void add_back(const int& e) {
-      Linked *now = new Linked(e);
-      if (empty()) {
-        Front = Back = now;
-      } else {
-        now->left = Back;
-        Back->right = now;
-        Back = now;
-      }
-      length += 1;
-    }
+    void add_back(const int& e);
 
-    /*
-     * removes the last element
-     * throws `runtime_error` exception if the list is empty
-     */
-    void remove_back() {
-      if (empty()) throw runtime_error("List is Empty!");
-      Linked *temp = Back;
-      Back = Back->left;
-      if (Back)
-        Back->right = NULL;
-      length -= 1;
-      free(temp);
-    }
+    void remove_back();
 
-    /*
-     * returns the number of items in the list
-     */
-    size_t get_size() const {
-      return length;
-    }
+    size_t get_size() const;
 
-    /*
-     * returns a string representation of the list in order.
-     * e.g., for a list [1,2,3,4], 
-     * 1 2 3 4
-     */
-    string to_string() {
-      string result = "";
-      Linked* now = Front;
-      while (now) {
-        result += ('0' + now->value);
-        now = now->right;
-      }
-      return result;
-    };
+    string to_string();
 
-    /*
-     * returns a string representation of the list in reverse order.
-     * e.g., for a list [1,2,3,4], 
-     * 4 3 2 1
-     */
-    string to_reverse_string() {
-      string result = "";
-      Linked* now = Back;
-      while (now) {
-        result += ('0' + now->value);
-        now = now->left;
-      }
-      return result;
-    };
+    string to_reverse_string();
 };
 #endif
