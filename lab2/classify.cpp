905e6761f924fb54c20c5811a50f832a5843e983
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-03-22 19:42:27 +0900

    Solve `Task 1`

--- lab2/classify.cpp
+++ lab2/classify.cpp
@@ -4,20 +4,25 @@
 
 using namespace std;
 
-
 void classify(void) {
+    freopen("input-classify.txt", "r", stdin);
+    freopen("output-classify.txt", "w", stdout);
 
-    ifstream fin;
-    ofstream fout;
-    
-    fin.open("input-classify.txt",std::fstream::in | std::fstream::out);
-   
-    
-    
-    fout.open("output-classify.txt", std::fstream::out);
-    
+    list <int> all[5];
+    int n;
+
+    // Read the input file and classify by mod 5
+    while (scanf("%d", &n) != EOF)
+        all[n % 5].push_back(n);
+
+    // Print each group
+    for (int i = 0; i < 5; i++) {
+        int length = all[i].size();
+        int cnt = 0;
+        for (int val: all[i])
+            printf("%d%c", val, " \n"[++cnt == length]);
+    }
 }
-
 
 int main(int argc, char* argv[]) {
     classify();
