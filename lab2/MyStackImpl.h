d15383be2b4c435e2c5c14da8becec85148268b7
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-03-30 06:12:59 +0900

    Update tab size

--- lab2/MyStackImpl.h
+++ lab2/MyStackImpl.h
@@ -1,59 +1,57 @@
-#include "MyStack.h"
 #include <stdexcept>
+#include "MyStack.h"
 
 using namespace std;
 
-template<typename T>
-class MyStackImpl : public MyStack<T>{
+template <typename T>
+class MyStackImpl : public MyStack<T> {
+ private:
+  size_t length;
+  size_t capacity;
 
-    private:
-        size_t length;
-        size_t capacity;
+  T* array;
 
-        T *array;
+  void resize(size_t new_capacity) {
+    T* new_array = new T[new_capacity];
+    for (size_t i = 0; i < length; i++)
+      new_array[i] = array[i];
+    swap(array, new_array);
+    // Remove the useless array
+    delete[] new_array;
+    capacity = new_capacity;
+  }
 
-        void resize(size_t new_capacity) {
-            T *new_array = new T[new_capacity];
-            for (size_t i = 0; i < length; i++)
-                new_array[i] = array[i];
-            swap(array, new_array);
-            // Remove the useless array
-            delete[] new_array;
-            capacity = new_capacity;
-        }
+ public:
+  MyStackImpl<T>() : MyStack<T>() {
+    capacity = 1;
+    length = 0;
+    array = new T[capacity];
+  }
 
-    public:
-        MyStackImpl<T>()  : MyStack<T>() {
-            capacity = 1;
-            length = 0;
-            array = new T[capacity];
-        }
+  T& top() const {
+    if (length == 0) throw runtime_error("No element to show – empty!");
+    return array[length - 1];
+  }
 
-        T& top() const {
-            if (length == 0) throw runtime_error("No element to show – empty!");
-            return array[length - 1];
-        }
+  size_t size() const {
+    return length;
+  }
 
-        size_t size() const {
-            return length;
-        }
+  void push(const T& e) {
+    array[length] = e;
+    length += 1;
+    if (capacity == length)
+      resize(capacity + capacity);
+  }
 
-        void push(const T& e) {
-            array[length] = e;
-            length += 1;
-            if (capacity == length)
-                resize(capacity + capacity);
-        }
+  void pop() {
+    if (length == 0) throw runtime_error("No element to pop – empty!");
+    length -= 1;
+    if (length > 8 && (length << 2) < capacity)
+      resize(capacity >> 2);
+  }
 
-        void pop() {
-            if (length == 0) throw runtime_error("No element to pop – empty!");
-            length -= 1;
-            if (length > 8 && (length << 2) < capacity)
-                resize(capacity >> 2);
-        }
-
-        ~MyStackImpl<T>()  {
-            delete[] array;
-        }
-
+  ~MyStackImpl<T>() {
+    delete[] array;
+  }
 };
--- lab2/classify.cpp
+++ lab2/classify.cpp
@@ -1,29 +1,29 @@
 #include <fstream>
-#include <list>
 #include <iostream>
+#include <list>
 
 using namespace std;
 
 void classify(void) {
-    freopen("input-classify.txt", "r", stdin);
-    freopen("output-classify.txt", "w", stdout);
+  freopen("input-classify.txt", "r", stdin);
+  freopen("output-classify.txt", "w", stdout);
 
-    list <int> all[5];
-    int n;
+  list<int> all[5];
+  int n;
 
-    // Read the input file and classify by mod 5
-    while (scanf("%d", &n) != EOF)
-        all[n % 5].push_back(n);
+  // Read the input file and classify by mod 5
+  while (scanf("%d", &n) != EOF)
+    all[n % 5].push_back(n);
 
-    // Print each group
-    for (int i = 0; i < 5; i++) {
-        int length = all[i].size();
-        int cnt = 0;
-        for (int val: all[i])
-            printf("%d%c", val, " \n"[++cnt == length]);
-    }
+  // Print each group
+  for (int i = 0; i < 5; i++) {
+    int length = all[i].size();
+    int cnt = 0;
+    for (int val : all[i])
+      printf("%d%c", val, " \n"[++cnt == length]);
+  }
 }
 
 int main(int argc, char* argv[]) {
-    classify();
+  classify();
 }
