69a71f6859f9c331f4837af58a5335b1487a33bd
Hyungon Moon <hyungon.moon@gmail.com>
2019-04-04 06:25:53 +0900

    deploying lab3

---
+++ lab3/MyTreeImpl.h
@@ -0,0 +1,29 @@
+#include "MyTree.h"
+#include <list>
+#include <stack>
+#include <queue>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class MyTreeImpl : public MyTree {
+
+    public:
+        MyTreeImpl() : MyTree() {
+        }
+
+        void load(string& input_name) {
+        }
+        string traverse_post(void) {
+        }
+        string traverse_pre(void) {
+        }
+        BigInt eval(string& assign_file_name) {
+            return 0;
+        }
+        void simplify(void) {
+        }
+};
