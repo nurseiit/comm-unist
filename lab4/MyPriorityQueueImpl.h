4d87281954ee65ee157c1fc151574d9ae871e458
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-04-24 19:29:46 +0900

    Update MyPriorityQueueImpl.h

--- lab4/MyPriorityQueueImpl.h
+++ lab4/MyPriorityQueueImpl.h
@@ -1,41 +1,63 @@
 #pragma once
-#include "MyPriorityQueue.h"
+#include <queue>
 #include <stdexcept>
 #include <vector>
-#include <queue>
+#include "MyPriorityQueue.h"
+
 using namespace std;
-template<typename T, bool isMax>
-/*
-if "isMax" is True, this will be the priority queue with maxHeap.
-otherwise, it will be with minHeap.
-You need to implement both. 
-*/
-using namespace std;
+
+template <typename T, bool isMax>
 class MyPriorityQueueImpl : public MyPriorityQueue<T, isMax> {
+  priority_queue<T> q;
+  T* array;
 
-priority_queue<T> q;
+ public:
+  MyPriorityQueueImpl<T, isMax>() : MyPriorityQueue<T, isMax>() {
+    while (!q.empty())
+      q.pop();
+    array = new T[2];
+  }
 
-public:
-	MyPriorityQueueImpl<T, isMax>() : MyPriorityQueue<T, isMax>() q() {
-	}
+  /*
+   * top:
+   * returns the reference of the first element.
+   * throws the runtime error if the queue is empty.
+   */
+  T& top() const {
+    return array[0];
+  }
 
-	T& top() const {
-            return q.top();
-	}
-	size_t size() const {
+  /*
+   * size:
+   * returns the number of elements in the queue.
+   */
+  size_t size() const {
+    return q.size();
+  }
 
-            return q.size();
-	}
-	bool empty() const {
-            return q.empty();
-	}
+  /*
+	 * empty:
+	 * returns whether the queue is empty or not.
+	 */
+  bool empty() const {
+    return q.empty();
+  }
 
-	void push(const T& e) {
-            q.push(e);
-	}
+  /*
+ 	 * push:
+ 	 * inserts the given item at the end of the queue.
+ 	 * throws the runtime_error if the queue is full
+ 	 */
+  void push(const T& e) {
+    q.push(isMax ? e : -e);
+  }
 
-	void pop() {
-            q.pop();
-	}
+  /*
+	 * pop:
+	 * removes the item at the beginning of the queue.
+	 * throws the runtime_error if the queue is empty
+	 */
+  void pop() {
+    q.pop();
+  }
 };
-
