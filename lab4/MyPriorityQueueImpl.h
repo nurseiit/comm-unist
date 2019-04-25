2f7729bc3ef0f628f5fc42062254e6b4c3651dcc
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-04-26 03:17:54 +0900

    Solve Priority

--- lab4/MyPriorityQueueImpl.h
+++ lab4/MyPriorityQueueImpl.h
@@ -5,17 +5,39 @@
 
 using namespace std;
 
-template <typename T, bool isMax, size_t cap>
+const int maxn = (1 << 20);
+const int offset = (1 << 19);
+
+class Fenwick {
+  int t[maxn];
+
+ public:
+  void upd(int r, int v = 1) {
+    r += offset;
+    for (; r < maxn; r |= r + 1)
+      t[r] += v;
+  }
+
+  int kth(int k) const {
+    int sum = 0, res = 0;
+    for (int i = maxn; i && i + res - 1 < maxn; i >>= 1)
+      if (sum + t[i + res - 1] <= k)
+        sum += t[i + res - 1], res += i;
+    return res - offset;
+  }
+};
+
+template <typename T, bool isMax, size_t capacity>
 class MyPriorityQueueImpl : public MyPriorityQueue<T, isMax> {
-  vector<T> array;
   size_t length;
   T* front;
+  Fenwick tree;
 
  public:
   MyPriorityQueueImpl<T, isMax>() : MyPriorityQueue<T, isMax>() {
-    array.clear();
     length = 0;
     front = new int[1];
+    tree = Fenwick();
   }
 
   /*
@@ -24,6 +46,8 @@ class MyPriorityQueueImpl : public MyPriorityQueue<T, isMax> {
    * throws the runtime error if the queue is empty.
    */
   T& top() const {
+    if (empty()) throw runtime_error("Empty!");
+    front[0] = tree.kth(isMax ? length - 1 : 0);
     return front[0];
   }
 
@@ -49,6 +73,8 @@ class MyPriorityQueueImpl : public MyPriorityQueue<T, isMax> {
  	 * throws the runtime_error if the queue is full
  	 */
   void push(const T& e) {
+    if (capacity == length) throw runtime_error("Can't push – full!");
+    tree.upd(e);
     length += 1;
   }
 
@@ -58,6 +84,8 @@ class MyPriorityQueueImpl : public MyPriorityQueue<T, isMax> {
 	 * throws the runtime_error if the queue is empty
 	 */
   void pop() {
+    if (empty()) throw runtime_error("Can't pop – empty!");
     length -= 1;
+    tree.upd(tree.kth(isMax ? length : 0), -1);
   }
 };
