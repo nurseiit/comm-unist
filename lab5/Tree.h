9f8a91bca4151f7583e19d4c8ded95450c9f1b3f
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-05-17 02:47:54 +0900

    Remove useles methods

--- lab5/Tree.h
+++ lab5/Tree.h
@@ -7,99 +7,39 @@
 
 using namespace std;
 
-/*
- * These two typedef are added to make to code more readable.
- */
-typedef long data_t;
-typedef long my_key_t;
+typedef long long int ll;
 
-struct Node_t {
-  my_key_t key;
-  data_t data;
-  Node_t *parent, *left, *right;
+struct Node {
+  ll key;
+  ll data;
+  Node *parent, *left, *right;
   short meta;  // To keep color for RB or balancing factor for AVL
+
+  Node(ll key, ll data, short meta = 0)
+    : key(key), data(data), meta(meta) {
+      parent = left = right = NULL;
+    }
+
+  Node(Node* parent, Node* left, Node* right)
+    : parent(parent), left(left), right(right) {}
 };
 
-/*
- * This struct is used for search to return two items.
- */
-struct Return_t {
-  data_t data;
+typedef Node* pNode;
+
+struct Data {
+  ll data;
   bool valid;
+
+  Data(ll data = 0, bool valid = false) 
+    : data(data), valid(valid) {}
 };
 
 class Tree_t {
   protected:
-    Node_t* root;
+    pNode root;
 
-    /*
-     * You don't need to implement the following protected functions,
-     * but are advised to do so and utilize them to implement the others.
-     *
-     * 
-     * When debugging, you may want to implement this and call
-     * whenever the tree is being modifies, to check if the tree satisfies
-     * the condition for bst.
-     */
-
-    /*
-     * A recursive search from a subtree
-     */
-    Node_t* search_subtree(Node_t* root, my_key_t key) const {
-      return NULL;
-    }
-
-    /*
-     * Recursive node removal.
-     */
-    void remove_node(Node_t* n) {
-      return;
-    }
-
-    /*
-     * Recursive node insertion
-     */
-    Node_t* insert_internal(my_key_t key, data_t data, short meta = 0) {
-      return NULL;
-    }
-
-    /*
-     * Recursive destruction for the destructor
-     */
-    void free_subtree(Node_t* r) {
-      if (r == NULL) return;
-      free_subtree(r->left);
-      free_subtree(r->right);
-      delete r;
-    }
-
-    string to_string(Node_t* n) {
-      if (!n) return string("EMPTY");
-      stringstream ret;
-      ret << "<" << n->key << "," << n->data << "> (" << n->meta << ")";
-      return ret.str();
-    }
-
-    /*
-     * Left or right rotate at a node. By implementing it here, you can
-     * use it in the other classes.
-     */
-    void rotate(Node_t* ni, bool left) {
-      return;
-    }
-
-    /*
-     * Recursive pre order traversal for grading uses this to learn about the tree structure.
-     */
-    string to_string_pre_order(Node_t* root) {
-      stringstream ret;
-      if (root) {
-        ret << to_string(root) << endl;
-        if (root->left) ret << to_string_pre_order(root->left);
-        if (root->right) ret << to_string_pre_order(root->right);
-      }
-      return ret.str();
-    }
+  string to_string(pNode node);
+  string to_string_pre_order(pNode node);
 
   public:
     Tree_t() {
@@ -112,20 +52,45 @@ class Tree_t {
      * When removing an internal node, we replace it with the smallest
      * one on its right subtree, to use a single reference output.
      */
-    void insert(my_key_t key, data_t data) {
-      return;
+    void insert(ll key, ll data) {
     }
 
-    bool remove(my_key_t key) {
+    /*
+     * Removes the key.
+     * Returns `true` if successful or `false`.
+     */
+    bool remove(ll key) {
       return true;
     }
 
-    Return_t search(my_key_t key) {
-      Return_t now = Return_t();
-      return now;
+    void search(ll key) {
     }
 
     string to_string_pre_order(void) {
+      return to_string_pre_order(root);
     }
 };
+
+/*
+ * [GRADER]
+ */
+string Tree_t::to_string(pNode node) {
+  if (!node) return string("EMPTY");
+  stringstream ret;
+  ret << "<" << node->key << "," << node->data << "> (" << node->meta << ")";
+  return ret.str();
+}
+
+/*
+ * [GRADER]
+ */
+string Tree_t::to_string_pre_order(pNode node) {
+  stringstream ret;
+  if (node) {
+    ret << to_string(node) << endl;
+    if (node->left) ret << to_string_pre_order(node->left);
+    if (node->right) ret << to_string_pre_order(node->right);
+  }
+  return ret.str();
+}
 #endif
