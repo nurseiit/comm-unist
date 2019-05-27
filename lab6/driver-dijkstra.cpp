10be1d1eee206b3f1baedb57d801f9d472cb8512
Hyungon Moon <hyungon.moon@gmail.com>
2019-05-27 09:34:14 +0900

    deploying lab6

--- lab6/driver-dijkstra.cpp
+++ lab6/driver-dijkstra.cpp
@@ -6,15 +6,7 @@
 #include <fstream>
 #include <list>
 #include <iterator>
-#include <algorithm>
 using namespace std;
-
-bool compare(const list<string>& a, const list<string>& b) {
-	if (a.size() == b.size()) {
-		return a < b;
-	}
-	return a.size() < b.size();
-}
 
 int main(int argc, char* argv[]) {
 	FlightMap flight_graph;
@@ -55,30 +47,15 @@ int main(int argc, char* argv[]) {
 	list<string>::const_iterator airport_iter2 = airport_dest.begin();
 
 	for (int i = 0; i < count; i++) {
-		ListVector routes = flight_graph.findShortestRoute(*airport_iter1, *airport_iter2);
-		if (routes.empty()) routes = { {*airport_iter1, *airport_iter2} };
+		list<string> routes = flight_graph.findShortestRoute(*airport_iter1, *airport_iter2);
+		if (routes.empty()) routes = {*airport_iter1, *airport_iter2};
 		airport_iter1++;
 		airport_iter2++;
 
-		if (routes.empty()) {
-			continue;
-		}
+		double distance = flight_graph.calcRouteDistance(routes);
 
-		sort(routes.begin(), routes.end(), compare);
-
-		double distance = flight_graph.calcRouteDistance(routes.front());
-
-		out = out + to_string(int(distance)) + "\n";
-
-		for (auto it = routes.begin(); it != routes.end(); it++) {
-			list<string> route = *it;
-			ostringstream stream;
-			copy(route.begin(), route.end(), ostream_iterator<string>(stream, " "));
-			out = out + stream.str() + "\n";
-		}
+		out = out + to_string(int(distance)) + " \n";
 	}
-
-	//cout << out;
 
 	ofstream fout(argv[4]);
 	fout << out;
