c8f70653b6709819d256b58d38bed95fbe999197
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-05-24 14:47:35 +0600

    ✨ Define CBN Interpreter class

--- princip-of-prog-lang/assignments/ass2/CBN/interpreter.py
+++ princip-of-prog-lang/assignments/ass2/CBN/interpreter.py
@@ -1,7 +1,17 @@
-def interpret(exp, env):
-    print(exp, env)
-    return 420
+class InterpreterCBN:
+    def __init__(self):
+        pass
+
+    def evaluate(self, exp, env):
+        if self._is_atom(exp):
+            return exp
+        return 420
+
+    def _is_atom(self, exp):
+        # atoms are simple strings
+        return isinstance(exp, str)
 
 
 def P(exp):
-    return lambda env: interpret(exp, env)
+    cbn = InterpreterCBN()
+    return lambda env: cbn.evaluate(exp, env)
