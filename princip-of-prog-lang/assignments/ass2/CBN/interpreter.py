0a205ec26c074803155a6f79fb437c91d5e6a3ef
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-05-24 19:06:56 +0600

    🎨 Add tests & fix sym=?

--- princip-of-prog-lang/assignments/ass2/CBN/interpreter.py
+++ princip-of-prog-lang/assignments/ass2/CBN/interpreter.py
@@ -8,7 +8,9 @@ class Primitive:
         def snd(x): return x[1]
         def is_unit(x): return x == '#u'
         def is_bool(x): return isinstance(x, bool)
-        def is_int(x): return isinstance(x, int)
+        def is_int(x): return isinstance(
+            x, int) and x is not True and x is not False
+
         def is_sym(x): return x != '#u' and isinstance(x, str)
 
         # todo: may be only functions not all callables?
@@ -17,12 +19,21 @@ class Primitive:
         # todo: may be list as well?
         def is_pair(x): return isinstance(x, tuple)
 
+        def sym_eq(foo, bar):
+            if not isinstance(foo, list) or not isinstance(bar, list):
+                raise ValueError('not-a-symbol')
+            if foo[0] != 'sym' or bar[0] != 'sym':
+                raise ValueError('not-a-symbol')
+            if not is_sym(foo[1]) or not is_sym(bar[1]):
+                raise ValueError('not-a-symbol')
+            return foo[1] == bar[1]
+
         self.primitives = {'+': op.add, '-': op.sub,
                            '*': op.mul, '/': op.floordiv,
                            '%': op.mod, '=': op.eq,
                            '!=': not_eq, '<': op.lt,
                            '<=': op.le, '>': op.gt,
-                           '>=': op.ge, 'sym=?': op.eq,
+                           '>=': op.ge, 'sym=?': sym_eq,
                            'fst': fst, 'snd': snd,
                            'unit?': is_unit, 'bool?': is_bool,
                            'int?': is_int, 'sym?': is_sym,
@@ -66,8 +77,6 @@ class Primitive:
     def _validate(self, op, *args):
         # check all args
         for arg in args:
-            if op == 'sym=?' and not isinstance(arg, str):
-                raise ValueError('not-a-symbol')
             if op in self.only_bool and arg is not True and arg is not False:
                 raise ValueError('not-a-boolean')
             if op in self.only_int and (not isinstance(arg, int) or arg is True or arg is False):
@@ -89,6 +98,11 @@ class Environment:
         return self.vars[var]
 
 
+class Procedure:
+    def __init__(self):
+        pass
+
+
 class InterpreterCBN:
     def __init__(self):
         self._env = Environment()
@@ -99,14 +113,18 @@ class InterpreterCBN:
 
         # save flk arguments
         self._env.update(zip(exp[1], env))
-        return self._evaluate(exp[2])
+        try:
+            result = self._evaluate(exp[2])
+        except ValueError as e:
+            result = str(e)
+        return result
 
     def _evaluate(self, exp):
-        print('# eval', exp)
-        if self._is_atom(exp):
-            return self._env[exp]
-        elif self._is_const(exp):
+        # print('# eval', exp)
+        if self._is_const(exp):
             return exp
+        elif self._is_atom(exp):
+            return self._env[exp]
         elif self._is_atom(exp[0]):
             if exp[0] == 'prim':
                 op = exp[1]
--- princip-of-prog-lang/assignments/ass2/CBN/tests/simple_test.py
+++ princip-of-prog-lang/assignments/ass2/CBN/tests/simple_test.py
@@ -2,9 +2,66 @@ from parser import parse
 from interpreter import P
 
 
-def test1():
-    assert P(parse('(flk () (app (lam x (prim + x 1)) 2))'))([]) == 3
+# def test_app_lam():
+#     assert P(parse('(flk () (app (lam x (prim + x 1)) 2))'))([]) == 3
 
 
-def test2():
-    assert P(parse('(flk (y) (app (lam x (prim + x y)) 2))'))([3]) == 5
+# def test_main_args():
+#     assert P(parse('(flk (y) (app (lam x (prim + x y)) 2))'))([3]) == 5
+
+
+'''
+    #u −−−> unit FLK
+    #t −−−> true FLK
+    23 −−−> 23 FLK
+    (sym captain) −−−> captain 
+'''
+
+
+def test_basic():
+    assert P(parse('(flk () #u)'))([]) == '#u'
+    assert P(parse('(flk () #t)'))([]) == True
+    assert P(parse('(flk () 23)'))([]) == 23
+    assert P(parse('(flk () (sym captain))'))([]) == 'captain'
+
+
+'''
+    (prim not #t) −−−→ false FLK
+    (prim int? 1) −−−→ true FLK
+    (prim int? #t) −−−→ false FLK
+    (prim + 1 2) −−−→ 3 FLK
+    (prim / 17 5) −−−→ 3 {integer division} FLK
+    (prim % 17 5) −−−→ 2 {integer remainder} FLK
+    (prim sym=? (sym captain) (sym captain)) −−−→ true FLK
+    (prim sym=? (sym captain) (sym abstraction)) −−−→ false
+'''
+
+
+def test_prim():
+    assert P(parse('(flk () (prim not #t))'))([]) == False
+    assert P(parse('(flk () (prim int? 1))'))([]) == True
+    assert P(parse('(flk () (prim int? #t))'))([]) == False
+    assert P(parse('(flk () (prim + 1 2))'))([]) == 3
+    assert P(parse('(flk () (prim / 17 5))'))([]) == 3
+    assert P(parse('(flk () (prim % 17 5))'))([]) == 2
+    assert P(parse('(flk () (prim sym=? (sym captain) (sym captain)))'))(
+        []) == True
+    assert P(parse('(flk () (prim sym=? (sym captain) (sym abstraction)))'))(
+        []) == False
+
+
+'''
+    (prim + 1) −−−→ error:wrong-number-of-args FLK
+    (prim + 1 2 3) −−−→ error:wrong-number-of-args FLK
+    (prim not 1) −−−→ error:not-a-boolean FLK
+    (prim + #t 1) −−−→ error:not-an-integer FLK
+    (prim / 1 0) −−−→ error:divide-by-zero
+'''
+
+
+def test_errors():
+    assert P(parse('(flk () (prim + 1))'))([]) == 'wrong-number-of-args'
+    assert P(parse('(flk () (prim + 1 2 3))'))([]) == 'wrong-number-of-args'
+    assert P(parse('(flk () (prim not 1))'))([]) == 'not-a-boolean'
+    assert P(parse('(flk () (prim + #t 1))'))([]) == 'not-an-integer'
+    assert P(parse('(flk () (prim / 1 0))'))([]) == 'divide-by-zero'
