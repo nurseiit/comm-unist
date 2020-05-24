17d410800bbac89bb7db13bdb33f7be6ade1b930
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-05-24 16:05:26 +0600

    ✨ Save flk args

--- princip-of-prog-lang/assignments/ass2/CBN/interpreter.py
+++ princip-of-prog-lang/assignments/ass2/CBN/interpreter.py
@@ -1,8 +1,19 @@
 class InterpreterCBN:
     def __init__(self):
-        pass
+        self._env = {}
 
-    def evaluate(self, exp, env):
+    def interpret(self, exp, env):
+        print('# intr', exp, env)
+
+        # save flk arguments
+        args = exp[1]
+        n = len(args)
+        for i in range(n):
+            self._env[args[i]] = env[i]
+        return self._evaluate(exp[2])
+
+    def _evaluate(self, exp):
+        print('# eval', exp)
         if self._is_atom(exp):
             return exp
         return 420
@@ -14,4 +25,4 @@ class InterpreterCBN:
 
 def P(exp):
     cbn = InterpreterCBN()
-    return lambda env: cbn.evaluate(exp, env)
+    return lambda env: cbn.interpret(exp, env)
