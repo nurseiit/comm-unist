958451c63395b3a388dceeec899f9a6e9bd1f295
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-05-24 17:31:23 +0600

    🎨 All primitives but proc? & pair?

--- princip-of-prog-lang/assignments/ass2/CBN/interpreter.py
+++ princip-of-prog-lang/assignments/ass2/CBN/interpreter.py
@@ -17,10 +17,13 @@ class InterpreterCBN:
         self._env = Environment()
 
         not_eq = lambda *x: not op.eq(*x)
-        fst = lambda *x: x[0]
-        snd = lambda *x: x[1]
+        def fst(x): return x[0]
+        def snd(x): return x[1]
         def is_unit(x): return x == '#u'
         def is_bool(x): return isinstance(x, bool)
+        def is_int(x): return isinstance(x, int)
+        def is_sym(x): return x != '#u' and isinstance(x, str)
+        # todo: proc? & pair?
 
         self.primitives = {'+': op.add, '-': op.sub,
                            '*': op.mul, '/': op.truediv,
@@ -29,15 +32,21 @@ class InterpreterCBN:
                            '<=': op.le, '>': op.gt,
                            '>=': op.ge, 'sym=?': op.eq,
                            'fst': fst, 'snd': snd,
-                           'unit?': is_unit, 'bool?': is_bool, }
+                           'unit?': is_unit, 'bool?': is_bool,
+                           'int?': is_int, 'sym?': is_sym,
+                           'not': op.not_, 'and': op.and_,
+                           'or': op.or_, 'bool=?': op.eq, }
 
         self.primitives_valid_argc = {
             '+': 2, '-': 2, '*': 2,
             '/': 2, '%': 2, '=': 2,
             '!=': 2, '<': 2, '<=': 2,
             '>': 2, '>=': 2, 'sym=?': 2,
-            'fst': 2, 'snd': 2,
-            'unit?': 1, 'bool?': 1, }
+            'fst': 1, 'snd': 1,
+            'unit?': 1, 'bool?': 1,
+            'int?': 1, 'sym?': 1,
+            'not': 1, 'and': 2, 'or': 2,
+            'bool=?': 2, }
 
     def interpret(self, exp, env):
         print('# intr', exp, env)
