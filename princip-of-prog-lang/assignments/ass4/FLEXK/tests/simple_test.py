25317fb32443054d7096d504a83be6291c518196
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-06-09 15:40:14 +0600

    ✅ Configure tests for vscode

--- princip-of-prog-lang/assignments/ass4/FLEXK/tests/simple_test.py
+++ princip-of-prog-lang/assignments/ass4/FLEXK/tests/simple_test.py
@@ -2,45 +2,45 @@ from parser import parse
 from checker import type_check
 
 
-def test1():
+def test_1():
     assert type_check(*parse('(flexk (()) 1): (=> () int)')) == True
 
 
-def test2():
+def test_2():
     assert type_check(*parse('(flexk (()) 1): (=> () bool)')) == False
 
 
-def test3():
+def test_3():
     assert type_check(*parse('''
     (flexk (()) (abs ((x int) (y int)) 1)) : (=> () (-> (int int) int))
     ''')) == True
 
 
-def test4():
+def test_4():
     assert type_check(*parse('''
     (flexk (()) (abs ((x int) (y int)) 1)) : (=> () (-> (int) int))
     ''')) == False
 
 
-def test5():
+def test_5():
     assert type_check(*parse('''
     (flexk ((x int)) 1) : (=> (int) int)
     ''')) == True
 
 
-def test6():
+def test_6():
     assert type_check(*parse('''
     (flexk ((x int)) 1) : (=> () int)
     ''')) == False
 
 
-def test7():
+def test_7():
     assert type_check(*parse('''
     (flexk ((x int) (y bool)) 1): (=> (int bool) int)
     ''')) == True
 
 
-def test8():
+def test_8():
     assert type_check(*parse('''
     (flexk ((x int) (y bool)) 1): (=> (int bool int) int)
     ''')) == False
